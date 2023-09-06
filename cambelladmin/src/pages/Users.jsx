127f389c21ef4b499cd031c5174e04ab5890c0fe
Mahendran Thanujan <73095554+ThanuMahee12@users.noreply.github.com>
2023-09-06 23:17:16 +0530

    userFilter add

--- cambelladmin/src/pages/Users.jsx
+++ cambelladmin/src/pages/Users.jsx
@@ -1,4 +1,4 @@
-import React,{  useEffect,useState} from 'react';
+import React,{ useState,useReducer} from 'react';
 import { Container,Row,Form,Col} from 'react-bootstrap';
 import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody,MDBInputGroup,MDBInput,MDBIcon} from 'mdb-react-ui-kit';
 import {FaUserSlash,FaUserCheck} from "react-icons/fa";
@@ -6,7 +6,7 @@ import axios from "axios";
 
 function UserRows ( { username, profile, isBlock, isOnline, email, religion, uid, mobile } ){
   return (
-      <tr>
+      <tr key={uid}>
         <td>
           <div className='d-flex align-items-center'>
             <img
@@ -29,7 +29,7 @@ function UserRows ( { username, profile, isBlock, isOnline, email, religion, uid
         isBlock ? ( <MDBBadge color='danger' pill>
           Blocked/{isOnline?"Online":"Offline"}
         </MDBBadge> ) : ( <MDBBadge color='warning' pill>
-            unBlocked
+            unBlocked/{isOnline?"Online":"Offline"}
           </MDBBadge>)
       }
         
@@ -38,29 +38,99 @@ function UserRows ( { username, profile, isBlock, isOnline, email, religion, uid
         <td>
           <MDBBtn color='link' rounded size='sm'>
             Edit
-          </MDBBtn>
-          <MDBBtn color='link' rounded size='sm'>
-            Block
-          </MDBBtn>
+        </MDBBtn>
+        {
+          isBlock?(   <MDBBtn color='link' rounded size='sm'>
+          UnBlock
+        </MDBBtn>):(<MDBBtn color='link' rounded size='sm'>
+          Block
+        </MDBBtn>)
+        }
+       
         </td>
       </tr>
   )
 }
 export default function Users ()
 {
-  const [ user, setUser ] = useState( [
+  const [searchText, setSearchText] = useState('');
+  const userdata = {
+   userEx: [
     {
       uid: 1212, username: "ThanuMahee",
-      mobile:"+9477746327424",email:""
-}
-  ] );
- 
+      mobile: "+9477746327424", email: "fdsfsdfds", isOnline: false, isBlock: false,
+      religion: "Hindu", profile: "https://static.thenounproject.com/png/363633-200.png"
+    }, {
+      uid: 1212, username: "ThanuMahee",
+      mobile: "+9477746327424", email: "fdsfsdfds", isOnline: true, isBlock: false,
+      religion: "Hindu", profile: "https://static.thenounproject.com/png/363633-200.png"
+    },
+    {
+      uid: 1212, username: "Yuka",
+      mobile: "+9477746327424", email: "fdsfsdfds", isOnline: true, isBlock: true,
+      religion: "Hindu", profile: "https://static.thenounproject.com/png/363633-200.png"
+    },
+    {
+      uid: 1212, username: "ThanuMahee",
+      mobile: "+9477746327424", email: "fdsfsdfds", isOnline: true, isBlock: false,
+      religion: "Hindu", profile: "https://static.thenounproject.com/png/363633-200.png"
+    },
+    {
+      uid: 1212, username: "ThanuMahee",
+      mobile: "+9477746327424", email: "fdsfsdfds", isOnline: true, isBlock: true,
+      religion: "Hindu", profile: "https://static.thenounproject.com/png/363633-200.png"
+    }
+    ],
+    showBlocked: false,
+    showUnblocked: false,
+  };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'TOGGLE_SHOW_BLOCKED':
+        return {
+          ...state,
+          showBlocked: !state.showBlocked,
+          showUnblocked: state.showBlocked ? state.showUnblocked : false,
+        };
+      case 'TOGGLE_SHOW_UNBLOCKED':
+        return {
+          ...state,
+          showUnblocked: !state.showUnblocked,
+          showBlocked: state.showUnblocked ? state.showBlocked : false,
+        };
+      default:
+        return state;
+    }
+  };
+  const [ user, dispatch ] = useReducer(reducer,userdata );
+  const filteredUsers = user.userEx.filter( ( userp ) =>
+  {
+    const nameIncludesSearchText = userp.username.toLowerCase().includes(searchText.toLowerCase()) || userp.email.toLowerCase().includes(searchText.toLowerCase());
+    if (user.showBlocked && !user.showUnblocked) {
+      return userp.isBlock;
+    } else if (!user.showBlocked && user.showUnblocked) {
+      return !userp.isBlock;
+    } else {
+      return nameIncludesSearchText;
+    }
+  });
+
+  const toggleShowBlocked = () => {
+    dispatch({ type: 'TOGGLE_SHOW_BLOCKED' });
+  };
+
+  const toggleShowUnblocked = () => {
+    dispatch({ type: 'TOGGLE_SHOW_UNBLOCKED' });
+  };
+  const handleSearchTextChange = (e) => {
+    setSearchText(e.target.value);
+  };
     return(
-    <Container className="position-relative">
-        <Row>
+    <Container className="position-relative vh-100">
+        <Row >
             <Col lg={4}>
             <MDBInputGroup>
-      <MDBInput label='Search' />
+      <MDBInput label='Search'   onChange={handleSearchTextChange} />
       <MDBBtn rippleColor='dark'>
         <MDBIcon icon='search' />
       </MDBBtn>
@@ -71,39 +141,35 @@ export default function Users ()
       <div>
           <Form.Check
             inline
-            label={<FaUserCheck/>}
-            name="group1"
+                  label={ <FaUserCheck /> }
+                  checked={user.showUnblocked}
+                  onChange={toggleShowUnblocked}
+     
           />
           <Form.Check
             inline
-            label={<FaUserSlash/>}
-            name="group1"
+            label={<FaUserSlash/>}  checked={user.showBlocked}
+            onChange={toggleShowBlocked}
+           
           />
         </div>
       </span>
             </Col>
         </Row>
-<MDBTable align='middle'>
+<MDBTable align='middle' >
       <MDBTableHead >
         <tr>
-          <th scope='col'>Profile</th>
-          <th scope='col'>User Name</th>
+          <th scope='col'>User</th>
+          <th scope='col'>Info</th>
           <th scope='col'>Status</th>
           <th scope='col'>Bookings</th>
           <th scope='col'>Actions</th>
         </tr>
       </MDBTableHead>
       <MDBTableBody>
-        {/* {
-          users.map(us=>( <UserRows/>))
-        } */}
-    
-      {/* <UserRows/>
-      <UserRows/>
-      <UserRows/>
-      <UserRows/>
-      <UserRows/>
-      <UserRows/> */}
+            {
+              filteredUsers.map( userdataset => ( <UserRows {...userdataset} />))
+        }
        
       </MDBTableBody>
     </MDBTable>
